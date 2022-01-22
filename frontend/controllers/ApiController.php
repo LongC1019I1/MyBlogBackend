a15293ded991d6399f7df5ff58eff772550945a8
longbt <longbt@lumi.biz>
2022-01-22 11:57:13 +0700

    first commit

---
+++ frontend/controllers/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\rest\ActiveController;
+
+class ApiController extends ActiveController
+
+{
+    const access_key = "da53fe41852c808128fe2c651543403d";
+    
+}
